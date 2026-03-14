9076f28ae952fe49f9f1897d1ce963fc6bccc2c8
Taha Khan <taha1112@gmail.com>
2026-03-13 20:25:14 -0700

    fix settings and revert back to post

--- frontend/src/api/backend/createDnsCredential.ts
+++ frontend/src/api/backend/createDnsCredential.ts
@@ -2,7 +2,7 @@ import * as api from "./base";
 import type { DnsCredential } from "./models";
 
 export async function createDnsCredential(item: { provider_id: string; credentials: string }): Promise<DnsCredential> {
-	return await api.put({
+	return await api.post({
 		url: "/nginx/dns-credentials",
 		data: item,
 	});
